c07a7afed5240d8d9be244bd33f47b090d583b17
WildxHV <hvsisodia02@gmail.com>
2024-01-11 16:22:04 +0530

    file compressing before zipping

--- home/serializers.py
+++ home/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from .models import Folder,Files
 import shutil
-
+from zipfile import ZipFile, ZIP_DEFLATED
+import pathlib
 class FileListSerializer(serializers.Serializer):
     files = serializers.ListField(
         child = serializers.FileField(max_length = 100000 , allow_empty_file = False , use_url = False)
@@ -9,7 +10,14 @@ class FileListSerializer(serializers.Serializer):
     folder = serializers.CharField(required = False)
     
     def zip_files(self,folder):
-        shutil.make_archive(f'public/media/zip/{folder}' , 'zip' ,f'public/media/{folder}' )
+        # Can use this to directly zip the folder or files
+        # shutil.make_archive(f'public/media/zip/{folder}' , 'zip' ,f'public/media/{folder}' )
+
+        # Using Zipfile to compress the files before creating the
+        directory = pathlib.Path(f"public/media/{folder}")
+        with ZipFile(f'public/media/zip/{folder}.zip', "w", ZIP_DEFLATED, compresslevel=9) as archive:
+            for file_path in directory.rglob("*"):
+                archive.write(file_path, arcname=file_path.relative_to(directory))
         
     def create(self , validated_data):
         folder = Folder.objects.create()
@@ -19,4 +27,5 @@ class FileListSerializer(serializers.Serializer):
             files_obj = Files.objects.create(folder = folder , file = file)
             files_objs.append(files_obj)
         self.zip_files(folder.uid)
+        
         return {'files' : {} , 'folder' : str(folder.uid)}
